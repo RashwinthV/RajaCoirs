96eda9132b17e6f91ad31a27ebafda16e34a5d79
tharunvikram71 <vrajamani741@gmail.com>
2025-05-29 19:04:39 +0530

    new commit

--- Backend/userService/server.js
+++ Backend/userService/server.js
@@ -6,16 +6,21 @@ const newuser=require('./Routes/RegisterRoute')
 const user=require('./Routes/userRoute')
 const connectDb=require('./Database/db')
 
-app.use(cors({
-  origin: "http://localhost:5173",
+
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://rajacoirs.onrender.com"],
   credentials: true,
-}));app.use(express.json());
+};
+app.use(cors(corsOptions));
+
+
+app.use(express.json());
 
 const port = process.env.PORT;
 app.use('/newuser',newuser)
 app.use('/user',user)
 
-connectDb()
+connectDb() 
 app.listen(port, () => {
   console.log(`User Service is runnning in port ${port}`);
 });
